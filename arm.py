1f2a321ce20cd5662aaa3c0a5bb70db0ec895ad5
kaushik-dhiman <43110808+kaushik-dhiman@users.noreply.github.com>
2020-07-29 10:47:06 +0530

    Add files via upload

---
+++ arm.py
@@ -0,0 +1,13 @@
+def armstrong(num):
+    sum = 0
+    temp = num
+    while temp > 0:
+       digit = temp % 10
+       sum += digit ** 3
+       temp //= 10
+    if num == sum:
+       print(num,"is an Armstrong number")
+    else:
+       print(num,"is not an Armstrong number")
+numb=int(input("Enter the number: "))       
+armstrong(numb)
